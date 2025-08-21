8b8ecf8437e448530a13d6586b86dc7bd2140c57
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-08-21 17:32:44 +0300

    rename

--- mathworks-servicehost/squashfs.sh
+++ mathworks-servicehost/squashfs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 TMP_DIR="$(mktemp -d)"
-mkdir -p $TMP_DIR/$VERSION
-unzip $ZIP_FILE -d $TMP_DIR/$VERSION
+mkdir -p $TMP_DIR/$MSH_VERSION
+unzip $ZIP_FILE -d $TMP_DIR/$MSH_VERSION
 {
-    echo "LatestDSInstallerVersion ${VERSION}";
-    echo "LatestDSInstallRoot ${ROOT_DIR}/${VERSION}";
-    echo "DSLauncherExecutable ${ROOT_DIR}/${VERSION}/bin/glnxa64/MathWorksServiceHost";
+    echo "LatestDSInstallerVersion ${MSH_VERSION}";
+    echo "LatestDSInstallRoot ${ROOT_DIR}/${MSH_VERSION}";
+    echo "DSLauncherExecutable ${ROOT_DIR}/${MSH_VERSION}/bin/glnxa64/MathWorksServiceHost";
 } >> $TMP_DIR/LatestInstall.info
 mksquashfs $TMP_DIR $SQFS_FILE
 rm -rf $TMP_DIR
