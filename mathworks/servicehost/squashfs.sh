549665d5ca7b01ed2105687abf08f37927e9a4e8
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-08-25 10:44:37 +0300

    updates

--- mathworks/servicehost/squashfs.sh
+++ mathworks/servicehost/squashfs.sh
@@ -8,5 +8,5 @@ unzip $ZIP_FILE -d $TMP_DIR/$MSH_VERSION
     echo "LatestDSInstallRoot ${ROOT_DIR}/${MSH_VERSION}";
     echo "DSLauncherExecutable ${ROOT_DIR}/${MSH_VERSION}/bin/glnxa64/MathWorksServiceHost";
 } >> $TMP_DIR/LatestInstall.info
-mksquashfs $TMP_DIR $SQFS_FILE
+mksquashfs $TMP_DIR $SQFS_FILE -all-root
 rm -rf $TMP_DIR
