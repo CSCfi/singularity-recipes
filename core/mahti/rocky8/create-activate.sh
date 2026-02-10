e4a7c162538441c9ba5edfdf5f89aa24f196ade0
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-02-10 12:18:04 +0200

    osu

---
+++ core/mahti/rocky8/create-activate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+env -i /projappl/project_2014802/spack/bin/spack env activate --sh -d /scratch/project_2014802/test/v1.1.0/core/gcc152_env/ > activate.sh
--- core/mahti/rocky8/run-osu.sh
+++ core/mahti/rocky8/run-osu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-srun --account=project_2001659 --partition=test --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:10:00 apptainer exec osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw
+srun --exclusive --account=project_2001659 --partition=test --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 apptainer run osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw
 
-srun --account=project_2001659 --partition=test --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:10:00 apptainer exec osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw; /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
+srun --exclusive --account=project_2001659 --partition=test --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 apptainer run osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
