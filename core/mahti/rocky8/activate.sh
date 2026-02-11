04ae96d54d058a8ac9ec5093f98a384204f8f65b
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-02-11 09:04:51 +0200

    updates

--- core/mahti/rocky8/activate.sh
+++
@@ -1,8 +0,0 @@
-export SPACK_ENV=/scratch/project_2014802/test/v1.1.0/core/gcc152_env;
-export SPACK_ENV_VIEW=default;
-alias despacktivate='spack env deactivate';
-export ACLOCAL_PATH=/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/share/aclocal:$ACLOCAL_PATH;
-export CMAKE_PREFIX_PATH=/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views:$CMAKE_PREFIX_PATH;
-export MANPATH=/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/share/man:/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/man:$MANPATH;
-export PATH=/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/bin:$PATH;
-export PKG_CONFIG_PATH=/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/share/pkgconfig:/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/lib64/pkgconfig:/scratch/project_2014802/test/v1.1.0/core/gcc152_env/views/lib/pkgconfig:$PKG_CONFIG_PATH;
--- core/mahti/rocky8/create-activate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-env -i /projappl/project_2014802/spack/bin/spack env activate --sh -d /scratch/project_2014802/test/v1.1.0/core/gcc152_env/ > activate.sh
