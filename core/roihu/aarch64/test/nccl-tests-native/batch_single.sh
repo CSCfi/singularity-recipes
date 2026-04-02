72aa4daa866b909aa4260c1a0f4fcae7d7ae84c5
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-02 13:18:24 +0300

    nccl tests

--- core/roihu/aarch64/test/nccl-tests-native/batch_single.sh
+++ core/roihu/aarch64/test/nccl-tests-native/batch_single.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=0 --nodes=1 --ntasks-per-node=1 --cpus-per-task=288 --gpus-per-task=4 --time=00:15:00
+#SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=0 --nodes=1 --ntasks-per-node=1 --cpus-per-task=288 --gpus-per-node=4 --time=00:15:00
 module purge
 module load gcc/14.3.0 openmpi/5.0.8 cuda/12.9.1 nccl/2.28.7-1
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$NCCL_INSTROOT/lib:$LD_LIBRARY_PATH
