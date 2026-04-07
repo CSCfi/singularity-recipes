6abe38e1baaca1e2a4db9f93eb67748848891394
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-07 10:49:30 +0300

    batch scripts

---
+++ core/roihu/aarch64/test/nccl-tests-container/batch_mpi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=0 --nodes=2 --ntasks-per-node=4 --cpus-per-task=72 --gpus-per-node=4 --time=00:15:00
+srun apptainer exec --nv nccl-tests.sif /opt/nccl-tests/build/all_reduce_perf_mpi -b 8 -e 128M -f 2 -g 1
