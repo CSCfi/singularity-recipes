74c49d636494839f4d6b2d5cb74bf30323c37f83
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-08 09:05:48 +0300

    makefile

--- core/roihu/x86_64/rocky9/oci/build-base.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-podman build -f base.Dockerfile -t base:v2026_03 --squash-all /
--- core/roihu/x86_64/rocky9/oci/build-spack-gcc-15.2.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-podman build -f spack-gcc-15.2.Dockerfile -t spack-gcc-15.2:v2026_03 /
