76198c1db8b15df6d24b05f9540324c4f79c2bfd
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-08 14:25:58 +0300

    osu benchmarks

--- core/roihu/x86_64/test/osu-native/install-osu.sh
+++ core/roihu/x86_64/test/osu-native/install-osu.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-
-source /usr/share/lmod/9.1.2/init/bash
 module purge
-module use /appl/modulefiles/spack/x86_64/Core
-module load gcc
-module load openmpi
-
-# Enable `gcc` and `binutils`
-export PATH=/appl/soft/spack/core/v2026_03/compilers_ec/install_dir/linux-x86_64_v4/gcc-15.2.0-5v4dg5kq2xj3we7lmfsobmfvnc2l5mut/bin:/appl/soft/spack/core/v2026_03/compilers_ec/install_dir/linux-x86_64_v4/binutils-2.45-qmicimoddwomguj7tl2fgkp55fupbjge/bin:$PATH
+module load gcc openmpi
 
 NPROCS=10
 OSU_MICRO_BENCHMARKS_VERSION=7.4
--- core/roihu/x86_64/test/osu-native/run-osu.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ./osu/osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard
-srun --exclusive --account=r_installation_spack --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 ./osu_bibw
-srun --exclusive --account=r_installation_spack --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 ./osu_latency
