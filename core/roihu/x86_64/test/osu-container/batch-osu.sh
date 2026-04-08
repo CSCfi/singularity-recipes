9129432b014bfdb4b329fd14be5b1dc3a1decd64
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-08 14:41:53 +0300

    osu

--- core/roihu/x86_64/test/osu-container/batch-osu.sh
+++ core/roihu/x86_64/test/osu-container/batch-osu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-#SBATCH --exclusive --account=r_installation_spack --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00
+#SBATCH --exclusive --account=project_2001659 --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00
 srun apptainer run osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
 srun apptainer run osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
--- core/roihu/x86_64/test/osu-native/batch-osu.sh
+++ core/roihu/x86_64/test/osu-native/batch-osu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#SBATCH --exclusive --account=r_installation_spack --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00
+#SBATCH --exclusive --account=project_2001659 --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00
 srun ./osu/osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/osu_bibw
 srun ./osu/osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/osu_latency
