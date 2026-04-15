ff64036954088cb0bbba6275df2fe5d25055c53c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-15 09:52:44 +0300

    update

--- core/roihu/x86_64/rocky9/oci/core-cpu-gcc-15.2.sh
+++ core/roihu/x86_64/rocky9/oci/core-cpu-gcc-15.2.sh
@@ -10,8 +10,8 @@ rsync --archive --mkpath --exclude="gcc-14.1.0-*" --exclude="gcc-14.3.0-*" --exc
 
 rsync --archive --mkpath /appl/soft/spack/core/v2026_03/x86_64/gcc152_ec "$ROOTFS/appl/soft/spack/core/v2026_03/x86_64"
 
-cat > $ROOTFS/opt/activate.sh << 'EOF'
-export PATH=/appl/soft/spack/core/v2026_03/x86_64/compilers_ec/install_dir/linux-zen5/binutils-2.46.0-cqyyzre4ylslq3peezmsn6vdg3exfkkp/bin:$PATH
+cat > $ROOTFS/opt/activate.sh << EOF
+export PATH=/appl/soft/spack/core/v2026_03/x86_64/compilers_ec/install_dir/linux-zen5/binutils-2.46.0-cqyyzre4ylslq3peezmsn6vdg3exfkkp/bin:\$PATH
 . /usr/share/lmod/9.1.2/init/bash
 module use /appl/modulefiles/spack/x86_64/v2026_03/Core
 module load gcc/15.2.0
