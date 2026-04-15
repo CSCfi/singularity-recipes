750d817f77bf1c1d6743115aed353e2a9f6bdb09
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-15 10:34:23 +0300

    fixes

--- core/roihu/aarch64/rocky9/oci/core-gpu-gcc-14.3-cuda-12.9.sh
+++ core/roihu/aarch64/rocky9/oci/core-gpu-gcc-14.3-cuda-12.9.sh
@@ -8,10 +8,10 @@ rsync --archive --mkpath --exclude="gcc/15.2.0*" --exclude="gcc/13.4.0*" --exclu
 
 rsync --archive --mkpath --exclude="gcc-15.2.0-*" --exclude="gcc-13.4.0-*" /appl/soft/spack/core/v2026_03/compilers_eg "$ROOTFS/appl/soft/spack/core/v2026_03"
 
-rsync --archive --mkpath /appl/soft/spack/core/v2026_03/g14cu129 "$ROOTFS/appl/soft/spack/core/v2026_03"
+rsync --archive --mkpath /appl/soft/spack/core/v2026_03/g14cu129_eg "$ROOTFS/appl/soft/spack/core/v2026_03"
 
 cat > $ROOTFS/opt/activate.sh << EOF
-export PATH=/appl/soft/spack/core/v2026_03/compilers_eg/install_dir/linux-neoverse_v2/binutils-2.45-iovy4emqxvyusjxcmgq6fendyvwibfxz/bin:$PATH \
+export PATH=/appl/soft/spack/core/v2026_03/compilers_eg/install_dir/linux-neoverse_v2/binutils-2.45-iovy4emqxvyusjxcmgq6fendyvwibfxz/bin:\$PATH \
 . /usr/share/lmod/9.1.2/init/bash \
 module use /appl/modulefiles/spack/aarch64/Core \
 module load gcc/14.3.0 \
