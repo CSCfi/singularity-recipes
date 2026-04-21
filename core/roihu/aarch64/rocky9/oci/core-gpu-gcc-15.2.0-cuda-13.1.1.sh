6ea9dde09b5e1ff1e8211b11822a939af84df8c8
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-21 09:40:39 +0300

    core-gpu-gcc-15.2.0-cuda-13.1.1

---
+++ core/roihu/aarch64/rocky9/oci/core-gpu-gcc-15.2.0-cuda-13.1.1.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu
+
+CONTAINER=$(buildah from localhost/core-gpu-deps:v2026_03)
+ROOTFS=$(buildah mount $CONTAINER)
+
+rsync --archive --mkpath --exclude="gcc/13.4.0*" --exclude="gcc/14.3.0*" --exclude="git-lfs/" --exclude="nvhpc/" --exclude="gromacs/" /appl/modulefiles/spack/aarch64/v2026_03 "$ROOTFS/appl/modulefiles/spack/aarch64"
+
+rsync --archive --mkpath --exclude="gcc-13.4.0-*" --exclude="gcc-14.3.0-*" /appl/soft/spack/core/v2026_03/aarch64/compilers_eg "$ROOTFS/appl/soft/spack/core/v2026_03/aarch64"
+
+rsync --archive --mkpath /appl/soft/spack/core/v2026_03/aarch64/g14cu129_eg "$ROOTFS/appl/soft/spack/core/v2026_03/aarch64"
+
+cat > $ROOTFS/opt/activate.sh << EOF
+export PATH=/appl/soft/spack/core/v2026_03/aarch64/compilers_eg/install_dir/neoverse_v2/gcc-11.5.0/binutils-2.45-iovy4e/bin:\$PATH
+. /usr/share/lmod/9.1.2/init/bash
+module use /appl/modulefiles/spack/aarch64/v2026_03/Core
+module load gcc/15.2.0
+module load cuda/13.1.1
+module load openblas/0.3.30
+module load openmpi/5.0.8
+EOF
+
+buildah unmount $CONTAINER
+
+buildah commit $CONTAINER localhost/core-gpu-gcc-15.2.0-cuda-13.1.1:v2026_03
+
+buildah tag localhost/core-gpu-gcc-15.2.0-cuda-13.1.1:v2026_03 satama.csc.fi/r_installation_spack/core-gpu-gcc-15.2.0-cuda-13.1.1:v2026_03
