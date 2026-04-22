7fd38e166915b6cdd2402c9c88050df66aae326d
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-22 13:56:18 +0300

    update container recipe

--- core/roihu/aarch64/rocky9/oci/core-gpu-gcc-14.3.0-cuda-12.9.1.sh
+++ core/roihu/aarch64/rocky9/oci/core-gpu-gcc-14.3.0-cuda-12.9.1.sh
@@ -4,16 +4,16 @@ set -eu
 CONTAINER=$(buildah from localhost/core-gpu-deps:v2026_03)
 ROOTFS=$(buildah mount $CONTAINER)
 
-rsync --archive --mkpath --exclude="gcc/15.2.0*" --exclude="gcc/13.4.0*" --exclude="git-lfs/" --exclude="nvhpc/" --exclude="gromacs/" /appl/modulefiles/spack/aarch64 "$ROOTFS/appl/modulefiles/spack"
+rsync --archive --mkpath --exclude="gcc/15.2.0*" --exclude="gcc/13.4.0*" --exclude="git-lfs/" --exclude="nvhpc/" --exclude="gromacs/" /appl/modulefiles/spack/aarch64/v2026_03 "$ROOTFS/appl/modulefiles/spack/aarch64"
 
 rsync --archive --mkpath --exclude="gcc-15.2.0-*" --exclude="gcc-13.4.0-*" /appl/soft/spack/core/v2026_03/compilers_eg "$ROOTFS/appl/soft/spack/core/v2026_03"
 
-rsync --archive --mkpath /appl/soft/spack/core/v2026_03/g14cu129_eg "$ROOTFS/appl/soft/spack/core/v2026_03"
+rsync --archive --mkpath /appl/soft/spack/core/v2026_03/aarch64/g14cu129_eg "$ROOTFS/appl/soft/spack/core/v2026_03/aarch64"
 
 cat > $ROOTFS/opt/activate.sh << EOF
 export PATH=/appl/soft/spack/core/v2026_03/compilers_eg/install_dir/linux-neoverse_v2/binutils-2.45-iovy4emqxvyusjxcmgq6fendyvwibfxz/bin:\$PATH
 . /usr/share/lmod/9.1.2/init/bash
-module use /appl/modulefiles/spack/aarch64/Core
+module use /appl/modulefiles/spack/aarch64/v2026_03/Core
 module load gcc/14.3.0
 module load cuda/12.9.1
 module load openblas/0.3.30
--- core/roihu/aarch64/test/nccl-tests-osu-container/batch_osu.sh
+++ core/roihu/aarch64/test/nccl-tests-osu-container/batch_osu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00
+#SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 --gpus-per-node=1
 # Use all four NICs per node with UCX_MAX_RNDV_LANES=4
 srun apptainer run --cleanenv --nv --env=UCX_MAX_RNDV_LANES=4 nccl-tests-osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
 srun apptainer run --cleanenv --nv --env=UCX_MAX_RNDV_LANES=4 nccl-tests-osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
