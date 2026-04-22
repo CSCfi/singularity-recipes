4c72182b4206c0a3944d9fd9f182625130b713b8
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-22 13:01:58 +0300

    update scripts

--- core/roihu/aarch64/test/nccl-tests-native/batch_mpi.sh
+++ core/roihu/aarch64/test/nccl-tests-native/batch_mpi.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-#SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=0 --nodes=2 --ntasks-per-node=4 --cpus-per-task=72 --gpus-per-node=4 --time=00:15:00
+#SBATCH --account=project_2001659
+#SBATCH --output=nccl-tests-mpi-%j.out
+#SBATCH --job-name=nccl-tests-mpi
+#SBATCH --partition=gpumedium
+#SBATCH --time=00:15:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=72
+#SBATCH --gpus-per-node=4
+#SBATCH --mem=0
+#SBATCH --exclusive
 module purge
-module load gcc/14.3.0 openmpi/5.0.8 cuda/12.9.1 nccl/2.28.7-1
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$NCCL_INSTROOT/lib:$OPENMPI_INSTROOT/lib:$LD_LIBRARY_PATH
+module load gcc/14.3.0
+module load openmpi/5.0.8
+module load cuda/12.9.1
+module load nccl/2.28.7-1
+module list
 srun ./nccl-tests/build/all_reduce_perf_mpi -b 8 -e 128M -f 2 -g 1
