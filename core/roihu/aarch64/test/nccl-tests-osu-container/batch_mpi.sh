af862e1347f1b74bee19d4a3143da36ed4cfa3c1
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-22 14:45:02 +0300

    fix

--- core/roihu/aarch64/test/nccl-tests-osu-container/batch_mpi.sh
+++ core/roihu/aarch64/test/nccl-tests-osu-container/batch_mpi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #SBATCH --exclusive --account=project_2001659 --partition=gpumedium --mem=0 --nodes=2 --ntasks-per-node=4 --cpus-per-task=72 --gpus-per-node=4 --time=00:15:00
-srun apptainer run --cleanenv --nv nccl-tests-osu.sif /opt/nccl-tests-2.18.3/build/all_reduce_perf_mpi -b 8 -e 128M -f 2 -g 1
+module purge
+srun apptainer run --nv nccl-tests-osu.sif /opt/nccl-tests-2.18.3/build/all_reduce_perf_mpi -b 8 -e 128M -f 2 -g 1
