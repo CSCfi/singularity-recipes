ca8a17c39365e12db50e98b5d07c8a44aafe338f
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-23 11:50:40 +0300

    core

---
+++ core/mahti/README.md
@@ -0,0 +1,8 @@
+Core base container for Mahti.
+
+```bash
+rpm -qa
+ls /etc/yum.repos.d
+ls /etc/pki/rpm-gpg
+ls /etc/pki/bull-rpm-gpg
+```
--- core/roihu/x86_64/test/osu-native/install-osu.sh
+++ core/roihu/x86_64/test/osu-native/install-osu.sh
@@ -10,5 +10,5 @@ cd ./osu
 wget -q http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-${OSU_MICRO_BENCHMARKS_VERSION}.tar.gz
 tar xf osu-micro-benchmarks-${OSU_MICRO_BENCHMARKS_VERSION}.tar.gz
 cd osu-micro-benchmarks-${OSU_MICRO_BENCHMARKS_VERSION}
-./configure CC=$(which mpicc) CXX=$(which mpicxx) CFLAGS=-O3
+./configure CC=mpicc CXX=mpicxx CFLAGS=-O3
 make -j${NPROCS}
