c30f308aa0ea124ee4c9f6ca7581524cf98e6489
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-27 09:59:50 +0300

    update base containers

--- core/roihu/aarch64/rocky9/oci/core-gpu-gcc-13.4.0-cuda-12.6.3.sh
+++ core/roihu/aarch64/rocky9/oci/core-gpu-gcc-13.4.0-cuda-12.6.3.sh
@@ -6,12 +6,12 @@ ROOTFS=$(buildah mount $CONTAINER)
 
 rsync --archive --mkpath --exclude="gcc/15.2.0*" --exclude="gcc/14.3.0*" --exclude="git-lfs/" --exclude="nvhpc/" --exclude="gromacs/" /appl/modulefiles/spack/aarch64/v2026_03 "$ROOTFS/appl/modulefiles/spack/aarch64"
 
-rsync --archive --mkpath --exclude="gcc-15.2.0-*" --exclude="gcc-14.3.0-*" /appl/soft/spack/core/v2026_03/compilers_eg "$ROOTFS/appl/soft/spack/core/v2026_03"
+rsync --archive --mkpath --exclude="gcc-15.2.0-*" --exclude="gcc-14.3.0-*" /appl/soft/spack/core/v2026_03/aarch64/compilers_eg "$ROOTFS/appl/soft/spack/core/v2026_03/aarch64"
 
 rsync --archive --mkpath /appl/soft/spack/core/v2026_03/aarch64/g13cu126_eg "$ROOTFS/appl/soft/spack/core/v2026_03/aarch64"
 
 cat > $ROOTFS/opt/activate.sh << EOF
-export PATH=/appl/soft/spack/core/v2026_03/compilers_eg/install_dir/linux-neoverse_v2/binutils-2.45-iovy4emqxvyusjxcmgq6fendyvwibfxz/bin:\$PATH
+export PATH=/appl/soft/spack/core/v2026_03/aarch64/compilers_eg/install_dir/neoverse_v2/gcc-11.5.0/binutils-2.45-iovy4e/bin:\$PATH
 . /usr/share/lmod/9.1.2/init/bash
 module use /appl/modulefiles/spack/aarch64/v2026_03/Core
 module load gcc/13.4.0
