9fe5a134eee35d620507a40686d6f5d70de70fa4
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-27 13:37:15 +0300

    refactoring

--- core/roihu/x86_64/test/osu-native/batch_osu.sh
+++ core/roihu/x86_64/test/osu-native/batch_osu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --account=project_2001659 --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 --exclusive
-srun ./osu/osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/osu_bibw
-srun ./osu/osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/osu_latency
+srun ./osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/osu_bibw
+srun ./osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/osu_latency
--- core/roihu/x86_64/test/osu-native/install.sh
+++ core/roihu/x86_64/test/osu-native/install.sh
@@ -6,8 +6,6 @@ module load openmpi
 NPROCS=10
 OSU_MICRO_BENCHMARKS_VERSION=7.4
 
-mkdir -p osu
-cd ./osu
 wget -q http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-${OSU_MICRO_BENCHMARKS_VERSION}.tar.gz
 tar xf osu-micro-benchmarks-${OSU_MICRO_BENCHMARKS_VERSION}.tar.gz
 cd osu-micro-benchmarks-${OSU_MICRO_BENCHMARKS_VERSION}
