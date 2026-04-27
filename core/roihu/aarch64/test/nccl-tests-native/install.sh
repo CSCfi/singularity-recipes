b1573d8ab5c7590ed495b8c3a15b814d5bd7bce0
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-27 12:48:39 +0300

    refcatoring

--- core/roihu/aarch64/test/nccl-tests-native/install.sh
+++ core/roihu/aarch64/test/nccl-tests-native/install.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
+module purge
+module load gcc/14.3.0
+module load openmpi
+module load cuda
+module load nccl
+module list
 git clone https://github.com/NVIDIA/nccl-tests.git nccl-tests
+cd nccl-tests
+make -j 10 CUDA_HOME=$CUDA_HOME NCCL_HOME=$NCCL_INSTROOT
+make -j 10 CUDA_HOME=$CUDA_HOME NCCL_HOME=$NCCL_INSTROOT MPI=1 MPI_HOME=$OPENMPI_INSTROOT NAME_SUFFIX=_mpi
