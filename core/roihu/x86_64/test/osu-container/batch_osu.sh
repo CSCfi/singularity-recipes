830fb32442c9d1a1303b9017edab3cf52af74196
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-08-20 11:16:20 +0300

    batch script

--- core/roihu/x86_64/test/osu-container/batch_osu.sh
+++ core/roihu/x86_64/test/osu-container/batch_osu.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #SBATCH --account=project_2001659 --partition=medium --mem=2G --nodes=2 --ntasks-per-node=1 --time=00:05:00 --exclusive
+module purge
 srun apptainer run osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
 srun apptainer run osu.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
